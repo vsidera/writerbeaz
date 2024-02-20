54c9d0a4ee02fec34c35b685e59e923879f5aa28
John Wambugu <john.wambugu@burnmfg.com>
2024-02-20 22:33:01 +0300

    rollback to 94

--- src/Components/Tutor/JobDetails.jsx
+++ src/Components/Tutor/JobDetails.jsx
@@ -21,11 +21,12 @@ const ProposalForm = ({ onSubmit, isSubmitting, priceError, proposal, setProposa
       <label className="block text-sm font-medium text-gray-700">
         Message to client
       </label>
-      <textarea
+      <input
+        type='textarea'
         name='proposal'
         className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-      ></textarea>
-
+        required
+      />
       <label className="block text-sm font-medium text-gray-700">
         Price
       </label>
@@ -49,7 +50,6 @@ const ProposalForm = ({ onSubmit, isSubmitting, priceError, proposal, setProposa
     </button>
   </form>
 );
-
 
 const SubmissionFile = ({ file, removeFile, downloadFile, submitted = false }) => {
   console.log("File: ", file);
